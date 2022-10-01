8e865932b33fb308abffdac91bbf739b6d0df327
Merlin Nguyen <jonhenry13241@gmail.com>
2022-10-02 02:41:36 +0700

    first build

--- YourMod.cs
+++ YourMod.cs
@@ -1,18 +1,42 @@
-using BepInEx;
+﻿using BepInEx;
 using BerryLoaderNS;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace YourMod
 {
-	[BepInPlugin("EpicExampleModId", "Epic Example Mod", "0.0.1")]
-	[BepInDependency("BerryLoader")]
-	class Plugin : BaseUnityPlugin
-	{
-		public static BepInEx.Logging.ManualLogSource L;
+    [BepInPlugin("EpicExampleModId", "Epic Example Mod", "0.0.1")]
+    [BepInDependency("BerryLoader")]
+    class Plugin : BaseUnityPlugin
+    {
+        public static BepInEx.Logging.ManualLogSource L;
 
-		private void Awake()
-		{
-			L = Logger;
-			L.LogInfo("hello from YourMod.Plugin.Awake");
-		}
-	}
+        private void Awake()
+        {
+            L = Logger;
+
+            var translationFilePath = Path.Combine(Directory.GetParent(this.Info.Location).FullName, "translation.tsv");
+
+            if (!File.Exists(translationFilePath))
+            {
+                L.LogError("translation.tsv missing!");
+            }
+            else
+            {
+                LocAPI.LoadTsvFromFile(translationFilePath);
+            }
+
+            var langs = SokLoc.Languages.ToList<SokLanguage>();
+            langs.Add(new SokLanguage { LanguageName = "Vietnamese", UnitySystemLanguage = SystemLanguage.Vietnamese });
+            SokLoc.Languages = langs.ToArray();
+
+            var langNamesField = typeof(SokLoc).GetField("localLanguageNames", BindingFlags.NonPublic | BindingFlags.Static);
+            Dictionary<string, string> langNames = (Dictionary<string, string>)langNamesField.GetValue(null);
+            langNames.Add("Vietnamese", "Tiếng Việt");
+            langNamesField.SetValue(null, langNames);
+        }
+    }
 }
